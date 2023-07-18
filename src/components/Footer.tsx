9de57d5c28aea1b17d6cf193c8dbbb1e0cd47490
Wender Barbosa <wender.barbosa@corp.lan>
2023-07-18 11:57:19 -0300

    ✅ Created Header, project and footer and start project (MVP)✅

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function Footer  ()  {
+  return (
+    <footer className="bg-gray-800 py-4 mt-8">
+      <div className="container mx-auto px-4">
+        <p className="text-center text-gray-500">© 2023 WenBarbosa. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header (){
+  return (
+    <nav className="top-0 sticky bg-gray-800 py-4 w-full">
+      <div className="container mx-auto px-4">
+        <h1 className="text-2xl text-white font-bold">WenBarbosa</h1>
+      </div>
+    </nav>
+  );
+};
+
