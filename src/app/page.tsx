a63257659b28d56c8576cd31208137099fce2299
Wender Barbosa <wender.barbosa@corp.lan>
2023-07-18 12:38:49 -0300

    ✅ Created first text/description for my portfolio ✅✅ ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,10 @@ const App = () => {
       <Header />
       <header className="bg-gray-100 py-8">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl text-gray-800 font-bold">Welcome to WenBarbosa!</h2>
-          <p className="text-gray-600 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis semper convallis. Sed ultrices orci nec leo rutrum, id placerat felis aliquet.</p>
+          <h2 className="text-3xl text-gray-800 font-bold">Let's create remarkable web experiences together!"</h2>
+          <p className="text-gray-600 mt-4">
+          Hello there! I'm a passionate Front-End Developer in production environment.  Currently focused on mastering React.js, Next.js, and TypeScript, I am continuously expanding my skill set to deliver top-notch user interfaces.  Let's create remarkable web experiences together!
+          </p>
           <a href="#" className="mt-4 inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Learn More</a>
         </div>
       </header>
@@ -20,5 +22,6 @@ const App = () => {
     </div>
   );
 };
+
 
 export default App;
