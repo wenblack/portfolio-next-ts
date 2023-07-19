09b2c7b4a3fda7ebebc3c696ee08ce896a9df559
Wender Barbosa <wender.barbosa@corp.lan>
2023-07-19 11:20:25 -0300

    ✅ Run lint and refact pages/component for build  ✅

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image";
 export function AboutMe () {
   return (
     <section className="bg-gray-200 py-8">
@@ -6,12 +6,12 @@ export function AboutMe () {
         <h2 className="text-3xl text-gray-800 font-bold mb-4">Who Am I?</h2>
         <div className="flex flex-col md:flex-row">
           <div className="md:w-1/2 md:pr-4">
-            <img src="https://github.com/wenblack.png" alt="Profile Image" className="rounded-full w-48 h-48 mx-auto md:mx-0 mb-4 md:mr-4" />
+            <Image src="https://github.com/wenblack.png" alt="Profile Image" className="rounded-full w-48 h-48 mx-auto md:mx-0 mb-4 md:mr-4" />
           </div>
           <div className="md:w-1/2">
-            <p className="text-gray-600">Hi there! I'm [Your Name], a passionate Front-End Developer with a keen eye for detail and a love for clean, elegant code. With [X] years of experience in web development, I specialize in crafting delightful user interfaces using modern web technologies.</p>
+            <p className="text-gray-600">Hi there! I&apos;m [Your Name], a passionate Front-End Developer with a keen eye for detail and a love for clean, elegant code. With [X] years of experience in web development, I specialize in crafting delightful user interfaces using modern web technologies.</p>
             <p className="text-gray-600 mt-4">My journey in web development began [mention your background and experience]. Throughout my career, I have honed my skills in React.js, Next.js, and TypeScript, allowing me to create responsive and performant web applications.</p>
-            <p className="text-gray-600 mt-4">When I'm not coding, you can find me exploring the latest trends in web design, attending tech conferences, and experimenting with new technologies. I am a strong believer in continuous learning and always strive to stay up-to-date with the ever-evolving web development landscape.</p>
+            <p className="text-gray-600 mt-4">When I&apos;m not coding, you can find me exploring the latest trends in web design, attending tech conferences, and experimenting with new technologies. I am a strong believer in continuous learning and always strive to stay up-to-date with the ever-evolving web development landscape.</p>
             <div className="flex flex-wrap mt-4">
               <a href="https://www.example.com" target="_blank" rel="noopener noreferrer" className="mr-4 mb-2 md:mb-0 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                 Twitter
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 export function Project  () {
@@ -7,21 +8,21 @@ export function Project  () {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {/* Project 1 */}
         <div className="bg-white p-4 shadow rounded">
-          <img src="path/to/project1-image.jpg" alt="Project 1" className="mb-4 rounded" />
+          <Image src="path/to/project1-image.jpg" alt="Project 1" className="mb-4 rounded" />
           <h4 className="text-lg text-gray-800 font-bold">Project 1</h4>
           <p className="text-gray-600 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis semper convallis.</p>
         </div>
 
         {/* Project 2 */}
         <div className="bg-white p-4 shadow rounded">
-          <img src="path/to/project2-image.jpg" alt="Project 2" className="mb-4 rounded" />
+          <Image src="path/to/project2-image.jpg" alt="Project 2" className="mb-4 rounded" />
           <h4 className="text-lg text-gray-800 font-bold">Project 2</h4>
           <p className="text-gray-600 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis semper convallis.</p>
         </div>
 
         {/* Project 3 */}
         <div className="bg-white p-4 shadow rounded">
-          <img src="path/to/project3-image.jpg" alt="Project 3" className="mb-4 rounded" />
+          <Image src="path/to/project3-image.jpg" alt="Project 3" className="mb-4 rounded" />
           <h4 className="text-lg text-gray-800 font-bold">Project 3</h4>
           <p className="text-gray-600 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis semper convallis.</p>
         </div>
