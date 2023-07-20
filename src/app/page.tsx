dcf8c39cde9fac641851c9f8d04965e4d9d5f45d
Wender Barbosa <wender.barbosa@corp.lan>
2023-07-20 12:41:38 -0300

    ✅ Created final AboutMe section Layout  ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,20 @@ import { SoftSkills } from '@/components/SoftSkills';
 
 const App = () => {
   return (
-    <div className=' flex flex-col justify-between min-h-screen'>
+    <div className=' flex flex-col justify-between mx-8 min-h-screen'>
       <Header />
       <header className="py-8">
         <div className="flex flex-col gap-8 items-center justify-center mx-auto px-4 h-[80vh]">
-          <h2 className="text-3xl  font-bold">Let&apos;s create remarkable web experiences together!</h2>
-          <p className="text-slate-400 mt-4">
+          <h2 className="text-3xl text-center font-bold">Let&apos;s create remarkable web experiences together!</h2>
+          <p className="text-slate-400 text-center mt-4">
           Trying to create captivating user experiences that combine aesthetics and functionality
           </p>
-          <a href="#" className="mt-4 inline-block bg-purple-600 hover:opacity-70 text-white font-bold py-2 px-4 rounded">Learn More</a>
+          <a href="#about" className="mt-4 inline-block bg-purple-600 hover:opacity-70 text-white font-bold py-2 px-4 rounded">Learn More</a>
         </div>
       </header>
-      <AboutMe/>
+      <section id='about' className='flex  self-center pt-16 w-full max-w-[800px] '>
+        <AboutMe/>
+      </section>
       <Project />
       <SoftSkills/>
       <Skills/>
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,19 +5,17 @@ import linkedInLogo from '@/assets/linkedin.svg'
 
 export function AboutMe () {
   return (
-    <section className="py-8">
-      <div className="container mx-auto px-6 py-1  ">
-        <div className="flex flex-col md:flex-row">
-          <div className="md:w-1/2 md:pr-4 mt-8">
-            <Image src={avatarImg} width={200}height={200} alt="Profile Image" className="rounded-full w-48 h-48 mx-auto md:mx-0 mb-4 md:mr-4" />
-          </div>
+    <section className="py-8 w-full bg-gray-800 rounded-lg">
+      <div className=" mx-auto px-6 py-1 bg-gray-800  ">
+        <div className="flex bg-gray-800 text-6xl mb-4 flex-col md:flex-row ">
+          👨‍💻
         </div>
-        <span className="text-purple-500 text-xl">Who i am</span>
-        <h2 className="text-3xl text-white mt-2 font-bold mb-4">Wender Barbosa</h2>
-        <h4 className=" text-lg text-gray-300">Frontend Developer </h4>
-          <div className="md:w-1/2 mt-8">
-            <p className="text-slate-300 text-justify">Hello there! I&apos;m passionate Front-End Developer in production environment.  Currently focused on mastering React.js, Next.js, and TypeScript, I&apos;m continuously expanding my skill set to deliver top-notch user interfaces.  Let&apos;s create remarkable web experiences together!</p>
-            <div className="flex flex-wrap mt-4">
+        <span className="bg-gray-800 text-purple-400  text-xl">Who i am</span>
+        <h2 className="bg-gray-800 text-3xl text-white mt-2 font-bold mb-4">Wender Barbosa</h2>
+        <h4 className=" bg-gray-800 text-lg text-gray-300">Frontend Developer </h4>
+          <div className=" bg-gray-800 mt-8">
+            <p className="bg-gray-800 text-slate-300  text-justify">Hello there! I&apos;m passionate Front-End Developer in production environment.  Currently focused on mastering React.js, Next.js, and TypeScript, I&apos;m continuously expanding my skill set to deliver top-notch user interfaces.  Let&apos;s create remarkable web experiences together!</p>
+            <div className="bg-gray-800 flex flex-wrap mt-4">
               <a href="https://www.example.com"  target="_blank" rel="noopener noreferrer" className="mr-4 px-6 py-1 mb-2 md:mb-0 bg-[#986DFF] hover:opacity-70  text-white font-bold rounded">
                 <Image className="bg-[#986DFF]" alt="LinkedIn Logo" width={25} height={25} src={linkedInLogo}/>
               </a>
